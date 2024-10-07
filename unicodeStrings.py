6455c2865e8d1e91e98d774b91efe9cc850be821
Mark Peters <mapeters101@earthlink.net>
2024-10-07 15:19:01 -0400

    Lesson062:  Work with Unicode up to 6:35.

---
+++ unicodeStrings.py
@@ -0,0 +1,28 @@
+print(ord('A'))
+print(chr(945))
+print(chr(128013))
+print(ord('α'))
+print(ord('🐍'))
+print(hex(ord('A')))
+print(hex(ord('α')))
+print(hex(ord('🐍')))
+
+print(int(3.14))
+print(int('3B1', 16))
+
+α = 'alpha'
+print(α)
+
+#🐍 = 100
+
+var = "\N{Latin Capital Letter A}lways look on the bright side of life."
+print(var)
+
+var = "\u0041lways look on the bright side of life."
+print(var)
+
+
+
+
+
+
